2c745ae16e2d9b3d8a1d8c4955f32aa891fd83ae
Phineas1500 <sriram.kiron@gmail.com>
2023-04-09 17:14:01 -0400

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Sriram Kiron on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
